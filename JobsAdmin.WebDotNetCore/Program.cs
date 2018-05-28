41a039da142084cb7263539443ef523636e71a69
Carlos Garcia <carlosggz@gmail.com>
2018-05-28 11:16:21 +0200

    Changed dotnet core configuration

--- JobsAdmin.WebDotNetCore/Program.cs
+++ JobsAdmin.WebDotNetCore/Program.cs
@@ -25,6 +25,8 @@ namespace JobsAdmin.WebDotNetCore
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseStartup<Startup>()
                 .Build();
     }
