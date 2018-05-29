f515ac05dc0f1cabc67e7c8de5ff81b7b6b3205e
Carlos Garcia <carlosggz@gmail.com>
2018-05-29 21:24:37 +0200

    Thread-safe notifications broker

--- JobsAdmin.Handler/NotificationsBroker.cs
+++ JobsAdmin.Handler/NotificationsBroker.cs
@@ -3,31 +3,42 @@ using JobsAdmin.Core.Dtos;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace JobsAdmin.Handler
 {
     internal class NotificationsBroker : INotificationsBroker, IJobsHandlerNotifier
     {
         private readonly List<IJobsHandlerNotifier> _notifiers = new List<IJobsHandlerNotifier>();
+        private readonly object _locker = new object();
 
         #region IJobsHandlerNotifier
 
         public void OnJobAdded(JobInfoDto jobInfo)
         {
-            foreach (var notifier in _notifiers)
-                notifier.OnJobAdded(jobInfo);
+            lock (_locker)
+            {
+                foreach (var notifier in _notifiers)
+                    Task.Run(() => notifier.OnJobAdded(jobInfo));
+            }
         }
 
         public void OnJobProgress(NotificationDto notification)
         {
-            foreach (var notifier in _notifiers)
-                notifier.OnJobProgress(notification);
+            lock (_locker)
+            {
+                foreach (var notifier in _notifiers)
+                    Task.Run(() => notifier.OnJobProgress(notification));
+            }
         }
 
         public void OnJobRemoved(string id)
         {
-            foreach (var notifier in _notifiers)
-                notifier.OnJobRemoved(id);
+            lock (_locker)
+            {
+                foreach (var notifier in _notifiers)
+                    Task.Run(() => notifier.OnJobRemoved(id));
+            }
         }
 
         #endregion
@@ -36,14 +47,20 @@ namespace JobsAdmin.Handler
 
         public void Subscribe(IJobsHandlerNotifier notifier)
         {
-            if (notifier != null && !_notifiers.Contains(notifier))
-                _notifiers.Add(notifier);
+            lock (_locker)
+            {
+                if (notifier != null && !_notifiers.Contains(notifier))
+                    _notifiers.Add(notifier);
+            }
         }
 
         public void Unsubscribe(IJobsHandlerNotifier notifier)
         {
-            if (notifier != null &&_notifiers.Contains(notifier))
-                _notifiers.Remove(notifier);
+            lock (_locker)
+            {
+                if (notifier != null && _notifiers.Contains(notifier))
+                    _notifiers.Remove(notifier);
+            }
         }
 
         #endregion
